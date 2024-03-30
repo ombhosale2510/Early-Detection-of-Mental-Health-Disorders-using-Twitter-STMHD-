1c9ce48afc9b8322c3558669b3a16b068005e728
Ben Chapman-Kish <ben.chapmankish@gmail.com>
2024-03-29 22:45:47 -0400

    Improve text cleaning and add more features for each tweet

--- preprocessing.py
+++ preprocessing.py
@@ -11,7 +11,6 @@
 
 import os
 import json
-import re
 import numpy as np
 import pandas as pd
 
@@ -30,28 +29,70 @@ from sklearn.metrics import (
     mean_poisson_deviance,
 )
 
+import re
+import string
+import wordninja
+import html
+from collections import OrderedDict
+
+import nltk
+nltk.download('stopwords', quiet=True)
+#nltk.download('wordnet')
+#nltk.download('omw-1.4')
+#nltk.download('punkt')
+
+from nltk.corpus import stopwords
+#from nltk.stem import PorterStemmer
+#from nltk.tokenize import TweetTokenizer
+#from nltk.stem import WordNetLemmatizer
+#from nltk.tokenize import word_tokenize
+
 from emot.emo_unicode import UNICODE_EMOJI
 from emot.emo_unicode import EMOTICONS_EMO
 
-def convert_emojis(text: str) -> str:
-    for emot in UNICODE_EMOJI:
-        text = text.replace(emot, "_".join(UNICODE_EMOJI[emot].replace(",","").replace(":","").split()))
+
+def process_pictograms(text: str, preserve: bool = True) -> str:
+    if preserve:
+        for emot in UNICODE_EMOJI:
+            text = text.replace(emot, "_".join(UNICODE_EMOJI[emot].replace(",","").replace(":","").split()))
+        for emot in EMOTICONS_EMO:
+            text = text.replace(emot, "_".join(EMOTICONS_EMO[emot].replace(",","").replace(":","").split()))
+    
+    else:
+        for emot in UNICODE_EMOJI:
+            text = text.replace(emot, "")
+        for emot in EMOTICONS_EMO:
+            text = text.replace(emot, "")
+    
     return text
 
-def replace_emoticons(text: str) -> str:
-    for emot in EMOTICONS_EMO:
-        text = text.replace(emot, "_".join(EMOTICONS_EMO[emot].replace(",","").replace(":","").split()))
-    return text
-
-def clean_text(orig_text: str) -> str:
+def clean_text(orig_text: str, preserve_pictograms: bool = True) -> str:
     # This function should clean up the text of each tweet to remove extra whitespace, punctuation, etc and convert emojis
     text = orig_text.strip() # first pass of whitespace trimming
 
+    text = process_pictograms(text, preserve_pictograms)
+
+    # these are taken directly from github.com/luisroberto-maker/MDD-and-PDD_Paper/blob/main/Pre-processing.ipynb
+    text = re.sub(r'_', ' ', text)
+    text = re.sub(r'$\w*', '', text)
+    text = re.sub(r'^RT[\s]+', '', text)
     text = re.sub(r'https?:\/\/.*[\r\n]*', '', text)
     text = re.sub(r'http', '', text)
+    text = re.sub(r'\@\w+|\#\…', '', text)
+    text = re.sub(r'…', '', text)
+    text = re.sub(r'[0-9]+', '', text)
+    text = re.sub(r'(.)\1+', r'\1\1', text)
+    text = ' '.join(OrderedDict((w,w) for w in text.split()).keys())
 
-    text = convert_emojis(text)
-    text = replace_emoticons(text)
+    text = html.unescape(text)
+
+    text.translate(str.maketrans('', '', string.punctuation))
+    translator = str.maketrans('', '', string.punctuation)
+    text = text.translate(translator)
+    text = wordninja.split(text)
+    text = " ".join(text)
+    text = text.lower()
+    text = " ".join(word for word in str(text).split() if word not in stopwords.words('english'))
     
     # TODO: any other text or punctuation that should be stripped?
 
@@ -62,6 +103,15 @@ def filter_tweet(text: str) -> bool:
     
     # TODO: implement more of a filter than just checking for the empty string
     return len(text) == 0
+
+def identify_anchor_tweet(anchor_text: str, candidate_text: str) -> bool:
+    anchor_text = clean_text(anchor_text, False)
+    candidate_text = clean_text(candidate_text, False)
+
+    anchor_stripped = " ".join(anchor_text.split()).strip().lower()
+    candidate_stripped = " ".join(candidate_text.split()).strip().lower()
+
+    return anchor_stripped == candidate_stripped
 
 NEGATIVE_PATH = "./data_sample/neg"
 ALL_DISORDERS = {
@@ -98,16 +148,20 @@ def create_tweets_df(disorders: list[str]) -> pd.DataFrame:
 
             for day_of_tweets, tweets_of_day in tweets_json.items():
                 for tweet in tweets_of_day:
-                    tweet_text_cleaned = clean_text(tweet["text"])
+                    tweet_text_raw = tweet["text"]
+                    tweet_text_cleaned = clean_text(tweet_text_raw)
 
                     if filter_tweet(tweet_text_cleaned):
                         continue # skip tweets that don't have alphanumeric characters
 
                     tweets_dicts.append({
                         "tweet_id": tweet["tweet_id"],
+                        "user_id": user_id,
+                        "tweet_text_raw": tweet_text_raw,
                         "tweet_text": tweet_text_cleaned,
                         "tweet_day": day_of_tweets,
-                        "pre_covid": None,
+                        "anchor_tweet": False,
+                        "pre_covid_anchor": None,
                         "disorder_flag": tweet["disorder_flag"],
                         "disorder": "negative"
                     })
@@ -120,6 +174,13 @@ def create_tweets_df(disorders: list[str]) -> pd.DataFrame:
                 if not user_id.isdigit():
                     continue # skip system files such as .DS_Store
 
+                user_anchor_tweet_file = f"{disorder_path}/{coviddir}/{user_id}/anchor_tweet.json"
+
+                with open(user_anchor_tweet_file, 'r') as f:
+                    tweet_json = json.load(f)
+                    anchor_tweet_text = tweet_json["anchor_tweet"]
+                    anchor_tweet_date = tweet_json["anchor_tweet_date"]
+
                 user_tweets_file = f"{disorder_path}/{coviddir}/{user_id}/tweets.json"
 
                 with open(user_tweets_file, 'r') as f:
@@ -127,22 +188,36 @@ def create_tweets_df(disorders: list[str]) -> pd.DataFrame:
 
                     for day_of_tweets, tweets_of_day in tweets_json.items():
                         for tweet in tweets_of_day:
-                            tweet_text_cleaned = clean_text(tweet["text"])
+                            tweet_text_raw = tweet["text"]
+                            tweet_text_cleaned = clean_text(tweet_text_raw)
+
+                            is_anchor_tweet = False
+                            if identify_anchor_tweet(anchor_tweet_text, tweet_text_raw):
+                                is_anchor_tweet = True
+
+                            if filter_tweet(tweet_text_cleaned):
+                                continue
 
                             tweets_dicts.append({
                                 "tweet_id": tweet["tweet_id"],
+                                "user_id": user_id,
+                                "tweet_text_raw": tweet_text_raw,
                                 "tweet_text": tweet_text_cleaned,
                                 "tweet_day": day_of_tweets,
-                                "pre_covid": (coviddir == "precovid"),
+                                "anchor_tweet": is_anchor_tweet,
+                                "pre_covid_anchor": (coviddir == "precovid"),
                                 "disorder_flag": tweet["disorder_flag"],
                                 "disorder": disorder
                             })
 
-    tweets_df = pd.DataFrame(tweets_dicts, columns=["tweet_id", "tweet_text", "tweet_day", "pre_covid", "disorder_flag", "disorder"])
+    tweets_df = pd.DataFrame(tweets_dicts, columns=[
+        "tweet_id", "user_id", "tweet_text_raw", "tweet_text", "tweet_day", "anchor_tweet", "pre_covid_anchor", "disorder_flag", "disorder"
+    ])
     tweets_df.set_index("tweet_id", inplace=True)
 
     return tweets_df
 
 if __name__ == "__main__":
-    print(create_tweets_df(["depression"]))
+    tweets_df = create_tweets_df(["depression"])
+    print(tweets_df[tweets_df.anchor_tweet.eq(True)])
 
